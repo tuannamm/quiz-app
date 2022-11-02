d84add658fcd07978b97af2feccf67ac2ecf200c
Tuan Nam <nttnnam@gmail.com>
2022-11-02 15:14:50 +0700

    lesson 6

---
+++ Chapter 1/Lesson 6/excercise.js
@@ -0,0 +1,5 @@
+const base_url = "localhost:8080";
+const api = "get-user";
+const fetch_page = 2;
+
+console.log(`${base_url}/${api}?page=${fetch_page}`);
---
+++ Chapter 1/Lesson 6/lesson6.js
@@ -0,0 +1,8 @@
+console.log("Hello\n" + "Tuan Nam");
+
+console.log(`hello
+Tuan Nam`);
+
+let a = 5;
+let b = 10;
+console.log(`Five plus ten is ${a + b} and not ${2 * a + b}.`);
