e200b6a7776e2b4db5dcfc344334639d5c2f4e76
Tuan Nam <nttnnam@gmail.com>
2022-11-02 18:37:06 +0700

    state

--- Chapter2/src/components/MyComponent.js
+++ Chapter2/src/components/MyComponent.js
@@ -7,12 +7,23 @@ class MyComponent extends React.Component {
     age: 21,
   };
 
+  handleClick(event) {
+    console.log("Hello World");
+    console.log("My name is " + this.state.name);
+  }
+
+  handleHover(event) {
+    console.log("Hover");
+  }
+
   //jsx
   render() {
     return (
       <>
         <div>
           My name is {this.state.name} and I'm from {this.state.address}
+          <button onClick={this.handleClick}>Click me</button>
+          <button onMouseOver={this.handleHover}>Hover me</button>
         </div>
       </>
     );
