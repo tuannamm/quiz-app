dbe7fed519686ffa39d0642d7a23ecab9114b798
Tuan Nam <nttnnam@gmail.com>
2022-11-10 13:30:43 +0700

    update

--- Chapter2/src/index.js
+++ Chapter2/src/index.js
@@ -7,15 +7,18 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import User from "./components/User/User";
 import Admin from "./components/Admin/Admin";
+import Test1 from "./components/test1/test1";
+import Test2 from "./components/test2/test2";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="users" element={<User />} />
-        <Route path="admins" element={<Admin />} />
+        <Route path="/" element={<App />}>
+          <Route path="/users" element={<User />} />
+          <Route path="/admins" element={<Admin />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   </Provider>
