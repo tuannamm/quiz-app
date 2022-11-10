1c6d9e5ebb1900ef80847b57f8cd0e8c5159d1b8
Tuan Nam <nttnnam@gmail.com>
2022-11-10 13:31:36 +0700

    update

--- Chapter2/src/index.js
+++ Chapter2/src/index.js
@@ -7,8 +7,6 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import User from "./components/User/User";
 import Admin from "./components/Admin/Admin";
-import Test1 from "./components/test1/test1";
-import Test2 from "./components/test2/test2";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
