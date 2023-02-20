704fefcadae81e10a8e1705bb7103aad78732a56
Tuan Nam <nttnnam@gmail.com>
2023-02-20 21:00:51 +0700

    install react-router-dom scss react-boostrap boostrap

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <button className="btn btn-primary">Click me</button>
+    </div>
+  );
+}
+
+export default App;
