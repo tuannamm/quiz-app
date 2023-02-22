17455472f41abff40a8b18e519fc998e81dd8900
Tuan Nam <nttnnam@gmail.com>
2023-02-22 20:48:21 +0700

    [ENHANCE] add video homepage and style navbar

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,5 +1,13 @@
+import videoHomepage from "../../assets/homepage.mp4";
+
 const HomePage = (props) => {
-  return <h1>HomePage</h1>;
+  return (
+    <div className="homepage-container">
+      <video autoPlay loop muted>
+        <source src={videoHomepage} type="video/mp4" />
+      </video>
+    </div>
+  );
 };
 
 export default HomePage;
