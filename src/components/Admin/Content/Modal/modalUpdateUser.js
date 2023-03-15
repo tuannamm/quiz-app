027b3b14ef15f88ec7852e3861a46f2d11a255b3
Tuan Nam <nttnnam@gmail.com>
2023-03-15 15:11:44 +0700

    [ENHANCE] install lodash, create modal update user, disable email and password field

---
+++ src/components/Admin/Content/Modal/modalUpdateUser.js
@@ -0,0 +1,174 @@
+import React, { useEffect, useState } from "react";
+import _ from "lodash";
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
+import "../ManageUser.scss";
+
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { postCreateNewUser } from "../../../../services/apiService";
+
+const ModalUpdateUser = ({
+  showModalUpdateUser,
+  setShowModalUpdateUser,
+  dataUpdate,
+}) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [username, setUsername] = useState("");
+  const [role, setRole] = useState("");
+  const [image, setImage] = useState("");
+  const [previewImg, setPreviewImg] = useState();
+
+  const handleClose = () => {
+    setShowModalUpdateUser(false);
+    setEmail("");
+    setPassword("");
+    setUsername("");
+    setImage("");
+    setPreviewImg("");
+  };
+
+  useEffect(() => {
+    if (!_.isEmpty(dataUpdate)) {
+      setEmail(dataUpdate.email);
+      setUsername(dataUpdate.username);
+      setRole(dataUpdate.role);
+      setImage("");
+      setPreviewImg(`data:image/jpeg;base64,${dataUpdate.image}`);
+    }
+  }, [dataUpdate]);
+
+  const handleUploadImage = (e) => {
+    if (e.target && e.target.files && e.target.files[0]) {
+      setPreviewImg(URL.createObjectURL(e.target.files[0]));
+      setImage(URL.createObjectURL(e.target.files[0]));
+    }
+  };
+
+  const validateEmail = (email) => {
+    return String(email)
+      .toLowerCase()
+      .match(
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      );
+  };
+
+  const handleSubmitUser = async () => {
+    // validate
+    const isValidEmail = validateEmail();
+    if (isValidEmail) {
+      toast.error("Invalid email");
+    }
+    if (!password) {
+      toast.error("Invalid password");
+    }
+
+    let data = await postCreateNewUser(email, password, username, role, image);
+
+    if (data && data.EC === 0) {
+      toast.success(data.EM);
+      handleClose();
+      // await getListUser();
+    }
+    if (data && data.EC !== 0) {
+      toast.error(data.EM);
+    }
+  };
+
+  return (
+    <>
+      <Modal
+        className="modal-add-user"
+        show={showModalUpdateUser}
+        onHide={handleClose}
+        size="xl"
+        centered
+        backdrop="static"
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>UPDATE NEW USERS</Modal.Title>
+        </Modal.Header>
+
+        <Modal.Body>
+          <form className="row g-3">
+            <div className="col-md-6">
+              <label className="form-label">Email</label>
+              <input
+                type="email"
+                className="form-control"
+                value={email}
+                disabled
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+            <div className="col-md-6">
+              <label htmlFor="inputPassword4" className="form-label">
+                Password
+              </label>
+              <input
+                type="password"
+                className="form-control"
+                value={password}
+                disabled
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+
+            <div className="col-md-6">
+              <label className="form-label">Username</label>
+              <input
+                type="text"
+                className="form-control"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+              />
+            </div>
+            <div className="col-md-6">
+              <label className="form-label">Role</label>
+              <select
+                className="form-select"
+                onChange={(e) => setRole(e.target.value)}
+                value={role}
+              >
+                <option selected value="User">
+                  USER
+                </option>
+                <option value="Admin">ADMIN</option>
+              </select>
+            </div>
+            <div className="col-md-12">
+              <label className="form-label label-upload" htmlFor="labelUpload">
+                Upload File Image
+              </label>
+              <input
+                type="file"
+                hidden
+                id="labelUpload"
+                onChange={(e) => handleUploadImage(e)}
+              />
+            </div>
+            <div className="col-md-12 image-preview">
+              {previewImg ? (
+                <img src={previewImg} alt="" />
+              ) : (
+                <span>Preview Image</span>
+              )}
+            </div>
+          </form>
+        </Modal.Body>
+
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            CLOSE
+          </Button>
+          <Button variant="primary" onClick={() => handleSubmitUser()}>
+            SAVE
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+};
+
+export default ModalUpdateUser;
