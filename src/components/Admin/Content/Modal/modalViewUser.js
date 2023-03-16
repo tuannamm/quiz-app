9fdb5e6bf56b0086aeb723558e03963e66e88085
Tuan Nam <nttnnam@gmail.com>
2023-03-16 11:38:27 +0700

    [ENHANCE] view user modal

---
+++ src/components/Admin/Content/Modal/modalViewUser.js
@@ -0,0 +1,133 @@
+import React, { useEffect, useState } from "react";
+import _ from "lodash";
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
+import "../ManageUser.scss";
+
+const ModalViewUser = ({
+  showModalViewUser,
+  setShowModalViewUser,
+  dataUpdate,
+  getListUser,
+}) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [username, setUsername] = useState("");
+  const [role, setRole] = useState("");
+  const [image, setImage] = useState("");
+  const [previewImg, setPreviewImg] = useState();
+
+  const handleClose = () => {
+    setShowModalViewUser(false);
+  };
+
+  useEffect(() => {
+    if (!_.isEmpty(dataUpdate)) {
+      setEmail(dataUpdate.email);
+      setUsername(dataUpdate.username);
+      setRole(dataUpdate.role);
+      setImage("");
+      setPreviewImg(`data:image/jpeg;base64,${dataUpdate.image}`);
+    }
+  }, [dataUpdate]);
+
+  const handleUploadImage = (e) => {
+    if (e.target && e.target.files && e.target.files[0]) {
+      setPreviewImg(URL.createObjectURL(e.target.files[0]));
+      setImage(URL.createObjectURL(e.target.files[0]));
+    }
+  };
+
+  return (
+    <>
+      <Modal
+        className="modal-add-user"
+        show={showModalViewUser}
+        onHide={handleClose}
+        size="xl"
+        centered
+        backdrop="static"
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>USER: {dataUpdate.username}</Modal.Title>
+        </Modal.Header>
+
+        <Modal.Body>
+          <form className="row g-3">
+            <div className="col-md-6">
+              <label className="form-label">Email</label>
+              <input
+                type="email"
+                className="form-control"
+                value={email}
+                disabled
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+            <div className="col-md-6">
+              <label htmlFor="inputPassword4" className="form-label">
+                Password
+              </label>
+              <input
+                type="password"
+                className="form-control"
+                value={password}
+                disabled
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+
+            <div className="col-md-6">
+              <label className="form-label">Username</label>
+              <input
+                type="text"
+                className="form-control"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                disabled
+              />
+            </div>
+            <div className="col-md-6">
+              <label className="form-label">Role</label>
+              <select
+                className="form-select"
+                onChange={(e) => setRole(e.target.value)}
+                value={role}
+                disabled
+              >
+                <option selected value="User">
+                  USER
+                </option>
+                <option value="Admin">ADMIN</option>
+              </select>
+            </div>
+            <div className="col-md-12">
+              <input
+                type="file"
+                hidden
+                id="labelUpload"
+                disabled
+                onChange={(e) => handleUploadImage(e)}
+              />
+            </div>
+            <div className="col-md-12 image-preview">
+              {previewImg ? (
+                <img src={previewImg} alt="" />
+              ) : (
+                <span>Preview Image</span>
+              )}
+            </div>
+          </form>
+        </Modal.Body>
+
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            CLOSE
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+};
+
+export default ModalViewUser;
